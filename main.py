b3b906ed65016a8cbb46a7cd61fd4077bcd1708f
themeinchow <themeinchow@gmail.com>
2022-11-28 14:07:19 +0100

    Add photos, created columns and description

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Brandon Chow")
+    content = """
+    Hi, my name is Brandon and I am a python programmer.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python.
+Feel free to contact me!
+"""
+
+st.write(content2)
