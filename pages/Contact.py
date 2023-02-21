6379d96528a62afe510893bd76b369134d34c51a
themeinchow <themeinchow@gmail.com>
2023-02-21 08:43:08 +0100

    Tweaks to email set up

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
-st.title("Contact Me")
+st.header("Contact Me")
 
-st.
+with st.form(key='email form'):
+    user_email = st.text_input('Your email address', key='email')
+    user_message = st.text_area('Your message')
+    button = st.form_submit_button()
+    message = f"""
+    Subject: New email from {user_email} \n\n
+    From: {user_email}
+    {user_message}
+    """
+
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully")
