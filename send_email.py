528945271528a7e25b010de8f8ea18aa3eaa61fb
themeinchow <themeinchow@gmail.com>
2023-02-21 08:42:00 +0100

    Set up emailing

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "themeinchow@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "themeinchow@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
